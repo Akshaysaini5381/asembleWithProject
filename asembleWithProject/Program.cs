694b273f874239a66f7e484553f959494402a13f
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-07-31 13:26:59 +0530

    Add project files.

---
+++ asembleWithProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary1;
+namespace asembleWithProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.sum(12, 3);
+            obj.mul(12, 10);
+            Console.WriteLine("save :");
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
